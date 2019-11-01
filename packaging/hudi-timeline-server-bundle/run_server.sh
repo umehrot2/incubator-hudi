d21e66b06065428399ce5900ecb1dc4b2ddb7e8d
Wenning Ding <wenningd@amazon.com>
2019-11-01 11:17:25 -0700

    Add patch01 to fix classpath for hudi timeline server

--- packaging/hudi-timeline-server-bundle/run_server.sh
+++ packaging/hudi-timeline-server-bundle/run_server.sh
@@ -18,11 +18,11 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 #Ensure we pick the right jar even for hive11 builds
-HOODIE_JAR=`ls -c $DIR/target/hudi-timeline-server-bundle-*.jar | grep -v test | head -1`
+HOODIE_JAR=`ls -c $DIR/../hudi-timeline-server-bundle-*.jar | grep -v test | head -1`
 
 if [ -z "$HADOOP_HOME" ]; then
-  echo "HADOOP_HOME not set. It must be set"
-  exit -1
+  echo "setting hadoop home dir"
+  HADOOP_HOME="/usr/lib/hadoop"
 fi
 
 if [ -z "$HADOOP_CONF_DIR" ]; then
@@ -30,9 +30,11 @@ if [ -z "$HADOOP_CONF_DIR" ]; then
   HADOOP_CONF_DIR="/etc/hadoop/conf"
 fi
 
+EMRFS_CONF_DIR="/usr/share/aws/emr/emrfs/conf"
+
 if [ ! -f "${DIR}/javax.servlet-api-3.1.0.jar" ]; then
  echo "Downloading runtime servlet jar first :"
- wget https://repo1.maven.org/maven2/javax/servlet/javax.servlet-api/3.1.0/javax.servlet-api-3.1.0.jar
+ sudo wget https://repo1.maven.org/maven2/javax/servlet/javax.servlet-api/3.1.0/javax.servlet-api-3.1.0.jar
 fi
 
 if [ ! -f "${DIR}/javax.servlet-api-3.1.0.jar" ]; then
@@ -40,13 +42,13 @@ if [ ! -f "${DIR}/javax.servlet-api-3.1.0.jar" ]; then
  exit -1
 fi
 
-HADOOP_COMMON_JARS=`ls -c ${HADOOP_HOME}/share/hadoop/common/*.jar | grep -v test | grep -v source | grep -v servlet | grep -v 'jetty-' | grep -v 'jersey-' | grep -v 'jsp-' | tr '\n' ':'`
-HADOOP_COMMON_LIB_JARS=`ls -c ${HADOOP_HOME}/share/hadoop/common/lib/*.jar | grep -v test | grep -v source | grep -v servlet | grep -v 'jetty-' | grep -v 'jersey-' | grep -v 'jsp-' | tr '\n' ':'`
-HADOOP_HDFS_JARS=`ls -c ${HADOOP_HOME}/share/hadoop/hdfs/*.jar | grep -v test | grep -v source | grep -v servlet | grep -v 'jetty-' | grep -v 'jersey-' | grep -v 'jsp-' | tr '\n' ':'`
-HADOOP_HDFS_LIB_JARS=`ls -c ${HADOOP_HOME}/share/hadoop/hdfs/lib/*.jar | grep -v test | grep -v source | grep -v servlet | grep -v 'jetty-' | grep -v 'jersey-' | grep -v 'jsp-' | tr '\n' ':'`
-HADOOP_JARS=${HADOOP_HOME}/share/hadoop/common/*:${HADOOP_HOME}/share/hadoop/mapreduce/*:${HADOOP_HOME}/share/hadoop/hdfs/*:${HADOOP_HOME}/share/hadoop/common/lib/*:${HADOOP_HOME}/share/hadoop/hdfs/lib/*
-HADOOP_JARS=${HADOOP_COMMON_JARS}:${HADOOP_COMMON_LIB_JARS}:${HADOOP_HDFS_JARS}:${HADOOP_HDFS_LIB_JARS}
+HADOOP_COMMON_JARS=`ls -c ${HADOOP_HOME}/*.jar | grep -v test | grep -v source | grep -v servlet | grep -v 'jetty-' | grep -v 'jersey-' | grep -v 'jsp-' | tr '\n' ':'`
+HADOOP_COMMON_LIB_JARS=`ls -c ${HADOOP_HOME}/lib/*.jar | grep -v test | grep -v source | grep -v servlet | grep -v 'jetty-' | grep -v 'jersey-' | grep -v 'jsp-' | tr '\n' ':'`
+HADOOP_HDFS_JARS=`ls -c /usr/lib/hadoop-hdfs/*.jar | grep -v test | grep -v source | grep -v servlet | grep -v 'jetty-' | grep -v 'jersey-' | grep -v 'jsp-' | tr '\n' ':'`
+HADOOP_HDFS_LIB_JARS=`ls -c /usr/lib/hadoop-hdfs/lib/*.jar | grep -v test | grep -v source | grep -v servlet | grep -v 'jetty-' | grep -v 'jersey-' | grep -v 'jsp-' | tr '\n' ':'`
+EMRFS_JARS=`ls -c /usr/share/aws/emr/emrfs/lib/*.jar /usr/share/aws/emr/emrfs/auxlib/*.jar | tr '\n' ':'`
+HADOOP_JARS=${HADOOP_COMMON_JARS}:${HADOOP_COMMON_LIB_JARS}:${HADOOP_HDFS_JARS}:${HADOOP_HDFS_LIB_JARS}:${EMRFS_JARS}
 
 SERVLET_JAR=${DIR}/javax.servlet-api-3.1.0.jar
-echo "Running command : java -cp ${SERVLET_JAR}:${HADOOP_JARS}:${HADOOP_CONF_DIR}:$HOODIE_JAR org.apache.hudi.timeline.service.TimelineService $@"
-java -Xmx4G -cp ${SERVLET_JAR}:${HADOOP_JARS}:${HADOOP_CONF_DIR}:$HOODIE_JAR org.apache.hudi.timeline.service.TimelineService "$@"
+echo "Running command : java -cp ${SERVLET_JAR}:${HADOOP_JARS}:${HADOOP_CONF_DIR}:${EMRFS_CONF_DIR}:$HOODIE_JAR org.apache.hudi.timeline.service.TimelineService $@"
+java -Xmx4G -cp ${SERVLET_JAR}:${HADOOP_JARS}:${HADOOP_CONF_DIR}:${EMRFS_CONF_DIR}:$HOODIE_JAR org.apache.hudi.timeline.service.TimelineService "$@"
