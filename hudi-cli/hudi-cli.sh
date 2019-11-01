85b2293f2e4cd896c704c0d09770de00e2e155f1
Wenning Ding <wenningd@amazon.com>
2019-11-01 11:17:02 -0700

    Add patch00 to fix hudi-cli classpath for emr

--- hudi-cli/hudi-cli.sh
+++ hudi-cli/hudi-cli.sh
@@ -17,12 +17,25 @@
 # limitations under the License.
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-HOODIE_JAR=`ls $DIR/target/hudi-cli-*.jar | grep -v source | grep -v javadoc`
+HOODIE_JAR=`ls $DIR/../hudi-cli-*.jar | grep -v source | grep -v javadoc`
 
 . "${DIR}"/conf/hudi-env.sh
 
+if [ -z "$SPARK_HOME" ]; then
+  echo "setting spark home"
+  export SPARK_HOME="/usr/lib/spark"
+fi
+if [ -z "$HADOOP_CONF_DIR" ]; then
+  echo "setting hadoop conf dir"
+  HADOOP_CONF_DIR="/etc/hadoop/conf"
+fi
+if [ -z "$SPARK_CONF_DIR" ]; then
+  echo "setting spark conf dir"
+  SPARK_CONF_DIR="/etc/spark/conf"
+fi
 if [ -z "$CLIENT_JAR" ]; then
   echo "Client jar location not set, please set it in conf/hudi-env.sh"
 fi
 
-java -cp ${HADOOP_CONF_DIR}:${SPARK_CONF_DIR}:$DIR/target/lib/*:$HOODIE_JAR:${CLIENT_JAR} -DSPARK_CONF_DIR=${SPARK_CONF_DIR} -DHADOOP_CONF_DIR=${HADOOP_CONF_DIR} org.springframework.shell.Bootstrap $@
+java -cp /usr/share/aws/emr/emrfs/conf:/usr/share/aws/emr/emrfs/lib/*:/usr/share/aws/emr/emrfs/auxlib/*:/usr/lib/hadoop/*:/usr/lib/spark/jars/*:${HADOOP_CONF_DIR}:${SPARK_CONF_DIR}:$DIR/../lib/*:$HOODIE_JAR:${CLIENT_JAR} -DSPARK_CONF_DIR=${SPARK_CONF_DIR} -DHADOOP_CONF_DIR=${HADOOP_CONF_DIR} org.springframework.shell.Bootstrap $@
+
